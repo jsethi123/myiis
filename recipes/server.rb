7f5cb45c03bd40cf8bfe8b2da10623f9e76fc6e9
jsethi123 <sarat931@gmail.com>
2017-12-18 23:48:36 +0000

    Add system info to server.rb and version to 0.1.2

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,8 +3,13 @@ powershell_script 'Install IIS' do
 end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-content '<h1>Hello, World!</h1>'
+  content "<h1>Hello, world!</h1>
+<h2>PLATFORM: #{node['platform']}</h2>
+<h2>HOSTNAME: #{node['hostname']}</h2>
+<h2>MEMORY:   #{node['memory']['total']}</h2>
+<h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
 end
+
 
 windows_service 'w3svc' do
   action [:enable, :start]
